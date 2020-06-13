63c638a2e10d59265e33161ca1412775942e6e61
OanaMariaB <oana.berentan@yahoo.com>
2020-06-13 22:01:43 +0100

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if Helpers.logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
 
-end
+  post '/signup' do
+    if params[:username]== "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(
+        username: params[:username], 
+        email: params[:email], 
+        password: params[:password]
+          )
+        session[:user_id] = @user.id
+        redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    if Helpers.logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if @user and @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/failure'
+    end
+
+  end
+  
+
+end #class
